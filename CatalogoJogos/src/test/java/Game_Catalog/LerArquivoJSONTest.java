eb39d7ba7a0456f363db7d967e201798fe8aabcf
ignorus <tiago.debs@gmail.com>
2018-12-12 23:36:44 -0200

    Começando a escrever aplicação utilizando TDD

---
+++ CatalogoJogos/src/test/java/Game_Catalog/LerArquivoJSONTest.java
@@ -0,0 +1,14 @@
+package Game_Catalog;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LerArquivoJSONTest {
+
+    @Test
+    @DisplayName("Lê arquivo JSON que cotém uma unica chave e valor")
+    void LeUmJSON()
+    {
+
+    }
+}
