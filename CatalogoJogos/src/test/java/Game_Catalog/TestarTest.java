c3f9dcf425d0cc4265a9b2fc411470ddd8adff47
ignorus <tiago.debs@gmail.com>
2018-12-13 16:52:57 -0200

    Teste JSONParse em arquivo com mais de uma chave

--- CatalogoJogos/src/test/java/Game_Catalog/TestarTest.java
+++ CatalogoJogos/src/test/java/Game_Catalog/TestarTest.java
@@ -1,9 +1,9 @@
 package Game_Catalog;
 
-import junit.framework.Assert;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +13,22 @@ import java.io.IOException;
 public class TestarTest {
 
     @Test
-    @DisplayName("Lê arquivo de teste JSON que cotém uma unica chave e valor")
+    @DisplayName("Lê arquivo de teste JSON que cotém duas chaves e um valor")
     void LeUmJSON() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
         FileReader leitor = new FileReader("C:/Users/RosaMaria/IdeaProjects/CatalogoJogos/src/files/json.json");
         JSONObject objetoJ = (JSONObject) parser.parse(leitor);
-        Assert.assertEquals("Tiago",objetoJ.get("nome"));
+        Assertions.assertEquals("Tiago",objetoJ.get("nome"));
+
+    }
+
+    @Test
+    @DisplayName("Lê arquivo de teste JSON que cotém duas chaves e um valor para cada chave")
+    void LeUmJSONComDuasChaves() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        FileReader leitor = new FileReader("C:/Users/RosaMaria/IdeaProjects/CatalogoJogos/src/files/json.json");
+        JSONObject objetoJ = (JSONObject) parser.parse(leitor);
+        Assertions.assertEquals("23",objetoJ.get("idade"));
 
     }
 }
