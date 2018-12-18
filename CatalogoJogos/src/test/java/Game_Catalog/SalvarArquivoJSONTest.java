ed5d56650554e388c122ec26f146324a0ea28102
ignorus <tiago.debs@gmail.com>
2018-12-18 16:37:29 -0200

    Começando TDD em SalvarArquivoJSON

--- CatalogoJogos/src/test/java/Game_Catalog/SalvarArquivoJSONTest.java
+++ CatalogoJogos/src/test/java/Game_Catalog/SalvarArquivoJSONTest.java
@@ -2,23 +2,25 @@ package Game_Catalog;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Rule;
 import org.junit.jupiter.api.*;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SalvarArquivoJSONTest {
 
-    JSONObject ler;
+    JSONObject lido;
     JSONParser parser;
     SalvarArquivoJSON salvarMock;
+    LerArquivoJSON arquivoMock;
+    JSONObject verificador;
 
     @Rule TemporaryFolder folder;
 
@@ -26,9 +28,11 @@ public class SalvarArquivoJSONTest {
     void iniciar() throws IOException {
         folder = new TemporaryFolder();
         folder.create();
-        ler = new JSONObject();
+        lido = new JSONObject();
+        verificador = new JSONObject();
         parser = new JSONParser();
         salvarMock = mock(new SalvarArquivoJSON().getClass());
+        arquivoMock = mock(new LerArquivoJSON().getClass());
 
     }
 
@@ -39,14 +43,25 @@ public class SalvarArquivoJSONTest {
         String plataforma = "Wii";
         String jogo = "MarioKart";
         String [] personagens = new String[]{"Mario","Luigi","Wario","Daisy","Peach"};
-        when(salvarMock.salvarJogoePersonagemJSON(ler,empresa,plataforma,jogo,personagens)).thenReturn("Jogo salvo");
-        Assertions.assertEquals("Jogo salvo",salvarMock.salvarJogoePersonagemJSON(ler,empresa,plataforma,jogo,personagens));
+        when(salvarMock.salvarJogoePersonagemJSON(lido,empresa,plataforma,jogo,personagens)).thenReturn("Jogo salvo");
+        assertEquals("Jogo salvo",salvarMock.salvarJogoePersonagemJSON(lido,empresa,plataforma,jogo,personagens));
     }
 
     @Test
-    @DisplayName("Salvar um JSON em arquivo, dado um objeto e quatro argumentos")
-    void SalvarJSONMultiNivelEmArquivo()
-    {
-
+    @DisplayName("Salvar um Jogo e seus Personagens, dado uma Empresa e Plataforma existentes")
+    void SalvarPersonagensEJogo() throws IOException, ParseException {
+        String empresa = "Sony";
+        String plataforma = "PS2";
+        String jogo = "Kingdom Hearts II";
+        String [] personagens = new String[]{"Sora","Roxas","Axel","Donald","Goofy",
+                "Riku","Kairi","Xemnas","Mickey","Mulan"};
+        when(arquivoMock.SepararDadosDoArquivo(arquivoMock.getArquivo())).
+                thenReturn((JSONObject) parser.parse(new FileReader("src/files/CatalogoJogos.Json")));
+        lido = arquivoMock.SepararDadosDoArquivo(arquivoMock.getArquivo());
+        SalvarArquivoJSON salvar = new SalvarArquivoJSON();
+        salvar.salvarJogoePersonagemJSON(lido,empresa,plataforma,jogo,personagens);
+        verificador = (JSONObject)((JSONObject)((JSONObject)lido.get(empresa)).get(plataforma)).get(jogo);
+        assertEquals("[\"Sora\",\"Roxas\",\"Axel\",\"Donald\",\"Goofy\"," +
+                " \"Riku\",\"Kairi\",\"Xemnas\",\"Mickey\",\"Mulan\"]",verificador.toString());
     }
 }
