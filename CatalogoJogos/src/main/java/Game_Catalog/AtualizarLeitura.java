63c315697cf701a891f5bc75dbad0a3b9d82adc9
ignorus <tiago.debs@gmail.com>
2018-12-19 17:42:14 -0200

    Regras de retorno validadas em teste

--- CatalogoJogos/src/main/java/Game_Catalog/AtualizarLeitura.java
+++ CatalogoJogos/src/main/java/Game_Catalog/AtualizarLeitura.java
@@ -1,11 +1,18 @@
 package Game_Catalog;
 
+import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
 public class AtualizarLeitura {
 
     public boolean LeituraAtualizada(String verificador)
     {
-        return TRUE;
+        if (verificador.equals("Jogo Salvo")) {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
     }
 }
--- CatalogoJogos/src/test/java/Game_Catalog/AtualizarLeituraTest.java
+++ CatalogoJogos/src/test/java/Game_Catalog/AtualizarLeituraTest.java
@@ -9,50 +9,67 @@ import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyObject;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class AtualizarLeituraTest {
 
+    AtualizarLeitura atualizadorMock;
     AtualizarLeitura atualizador;
     SalvarArquivoJSON salvar;
     String verificador;
     JSONObject objetoParametro;
+    String[] mockParametro;
 
     @BeforeEach
     void iniciar() throws IOException {
-        atualizador = mock(new AtualizarLeitura().getClass());
+        atualizadorMock = mock(new AtualizarLeitura().getClass());
         salvar = mock(new SalvarArquivoJSON("test").getClass());
         objetoParametro = new JSONObject();
+        mockParametro = new String[]{"oi","alo"};
+        atualizador = new AtualizarLeitura();
     }
 
     @Test
     @DisplayName("Atualiza a leitura de um mock, após algo ser escrito nele")
     void AtualizaMock()
     {
-        when(atualizador.LeituraAtualizada(verificador)).thenReturn(TRUE);
-        assertTrue(atualizador.LeituraAtualizada(verificador));
+        when(atualizadorMock.LeituraAtualizada(verificador)).thenReturn(TRUE);
+        assertTrue(atualizadorMock.LeituraAtualizada(verificador));
     }
 
     @Test
     @DisplayName("Dado um arquivo mock salvo, atualizar")
     void SalvoAtualizarMock() throws IOException {
-        String[] mockParametro = new String[]{"oi","alo"};
+
         when(salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro)).thenReturn("Jogo Salvo");
         verificador = salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro);
-        when(atualizador.LeituraAtualizada(verificador)).thenReturn(TRUE);
-        assertTrue(atualizador.LeituraAtualizada(verificador));
+        when(atualizadorMock.LeituraAtualizada(verificador)).thenReturn(TRUE);
+        assertTrue(atualizadorMock.LeituraAtualizada(verificador));
     }
 
     @Test
     @DisplayName("Dado um arquivo mock salvo sem alterações, não atualizar")
     void SalvoNaoAtualizarMock() throws IOException {
-        String[] mockParametro = new String[]{"oi","alo"};
         when(salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro)).thenReturn("Jogo já existe");
         verificador = salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro);
-        when(atualizador.LeituraAtualizada(verificador)).thenReturn(FALSE);
+        when(atualizadorMock.LeituraAtualizada(verificador)).thenReturn(FALSE);
+        assertFalse(atualizadorMock.LeituraAtualizada(verificador));
+    }
+
+    @Test
+    @DisplayName("Dada um arquivo mock, testar funcionalidade de atualizar")
+    void  SalvoAtualizar() throws IOException {
+        when(salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro)).thenReturn("Jogo Salvo");
+        verificador = salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro);
+        assertTrue(atualizador.LeituraAtualizada(verificador));
+    }
+
+    @Test
+    @DisplayName("Dada um arquivo mock, testar funcionalidade de não atualizar")
+    void  SalvoNaoAtualizar() throws IOException {
+        when(salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro)).thenReturn("Jogo já existe");
+        verificador = salvar.salvarJogoePersonagemJSON(objetoParametro,"a","b","c",mockParametro);
         assertFalse(atualizador.LeituraAtualizada(verificador));
     }
 }
