060e1c492cf55a9bd2f35aa1f7180c25263fc72f
ignorus <tiago.debs@gmail.com>
2018-12-20 16:33:41 -0200

    Começo TDD Interação com usuario

--- CatalogoJogos/src/main/java/Game_Catalog/App.java
+++ CatalogoJogos/src/main/java/Game_Catalog/App.java
@@ -1,10 +1,20 @@
 package Game_Catalog;
 
+import org.json.simple.JSONObject;
+
 public class App {
 
-    String caminho;
+
+    public static String caminho;
+    public static JSONObject objetoCatalgo;
+
     static void  main(String[] args)
     {
+
+        if (args[0].equalsIgnoreCase("ler"))
+        {
+             objetoCatalgo = new JSONObject();
+        }
 
     }
 }
--- CatalogoJogos/src/test/java/Game_Catalog/AppTest.java
+++ CatalogoJogos/src/test/java/Game_Catalog/AppTest.java
@@ -1,14 +1,32 @@
 package Game_Catalog;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class AppTest {
+
+    App IHM;
+    String[] parametroLer;
+
+    @BeforeEach
+    void iniciar()
+    {
+        parametroLer = new String[]{"ler"};
+    }
 
     @Test
     @DisplayName("Pega input para ler arquivo")
     void InputLer()
     {
-
+        IHM.main(parametroLer);
+        assertNotNull(IHM.objetoCatalgo);
     }
 }
