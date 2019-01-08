55b22fa635272ffca532de32318c566a7d553a1a
ignorus <tiago.debs@gmail.com>
2019-01-08 09:46:23 -0200

    Iniciando programa com HSQL

---
+++ CatalgoHSQL/src/test/java/testarTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class testarTest {
+
+    @Test
+    @DisplayName("Aprendendo HSQL")
+    void HSQLTest()
+    {}
+}
