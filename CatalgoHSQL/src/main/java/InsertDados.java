efad7b7f13c1e0f1d391b958132522c9c986232b
ignorus <tiago.debs@gmail.com>
2019-01-13 19:56:09 -0200

    Começando a adicionar objetos em tabelas

---
+++ CatalgoHSQL/src/main/java/InsertDados.java
@@ -0,0 +1,2 @@
+public class InsertDados {
+}
--- CatalgoHSQL/src/test/java/InsertDadosTest.java
+++ CatalgoHSQL/src/test/java/InsertDadosTest.java
@@ -1,10 +1,16 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class InsertDadosTest {
 
+    InsertDados adicionar = new InsertDados();
     @Test
     @DisplayName("Adicionar empresa ao DB")
     void addEmpresaTest()
-    {}
+    {
+        assertEquals(1,adicionar.empresa("Nintendo"));
+    }
 }
