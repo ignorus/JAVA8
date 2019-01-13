402d47b66d34aecdf6741a9ba5384a44d3716754
ignorus <tiago.debs@gmail.com>
2019-01-13 19:37:15 -0200

    Garantir conexão com DB

--- CatalgoHSQL/src/main/java/App.java
+++ CatalgoHSQL/src/main/java/App.java
@@ -1,5 +1,8 @@
 public class App {
 
     public static void main(String[] args)
-    {}
+    {
+        AcessoDB entrar = new AcessoDB();
+        entrar.conectar();
+    }
 }
---
+++ CatalgoHSQL/src/test/java/InsertDadosTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InsertDadosTest {
+
+    @Test
+    @DisplayName("Adicionar empresa ao DB")
+    void addEmpresaTest()
+    {}
+}
