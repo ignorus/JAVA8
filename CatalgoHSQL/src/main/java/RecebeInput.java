dde22f95d72ac09040edd7c2003df3d9d12d4fb9
rosa bugan <rosa.maria.bugan@accenture.com>
2019-01-15 07:54:57 -0200

    integração com usuario, LER ou SALVAR

--- CatalgoHSQL/src/main/java/RecebeInput.java
+++ CatalgoHSQL/src/main/java/RecebeInput.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class RecebeInput {
+
+    String resultado;
 
     public String LerouSalvar()
     {
-        return "oi";
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+        resultado = input.next();
+        if (resultado.equalsIgnoreCase("ler") || resultado.equalsIgnoreCase("salvar"))
+        {
+            return resultado;
+        }
+        return "Invalido";
     }
 }
